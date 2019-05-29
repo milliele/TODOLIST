ef149d50cb1494ecbfbdab40f8ba05d021b0b252
黄乐玫 <milliele@qq.com>
2019-05-29 23:09:52 +0800

    1. 修改以支持在xampp上部署

--- todolist/urls.py
+++ todolist/urls.py
@@ -16,6 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^list-to-do/', include('work.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/', include('work.urls')),
 ]
